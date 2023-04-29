65376c75f0725c75b6f12de31ad31b18371c20f6
Runyasak Chaengnaimuang <runyasak.c@gmail.com>
2023-04-29 23:41:30 +0700

    update getFirstPokemon spec

--- 10-getFirstPokemon/getFirstPokemon.spec.ts
+++ 10-getFirstPokemon/getFirstPokemon.spec.ts
@@ -1,23 +1,24 @@
-import { expect, test, vi } from 'vitest'
-import { getFirstPokemon } from './getFirstPokemon'
-import axios from 'axios'
-import { Mock } from 'vitest'
+// For lesson
 
-vi.mock('axios')
+// vi.mock('axios')
 
-test('should get first pokemon', async () => {
-  const expectedResult = { name: 'Pikachu' }
-  const mockPokemons = {
-    results: [{ name: 'Pikachu' }, { name: 'Ditto' }]
-  }
+// const mockAxios = axios.get as Mock
 
-  const mockAxios = axios.get as Mock
+// beforeEach(() => {
+//   mockAxios.mockReset()
+// })
 
-  mockAxios.mockResolvedValue({
-    data: mockPokemons
-  })
+// test('should get first pokemon', async () => {
+//   const expectedResult = { name: 'Pikachu' }
+//   const mockPokemons = {
+//     results: [{ name: 'Pikachu' }, { name: 'Ditto' }]
+//   }
 
-  const actualResult = await getFirstPokemon()
+//   mockAxios.mockResolvedValue({
+//     data: mockPokemons
+//   })
 
-  expect(actualResult).toEqual(expectedResult)
-})
+//   const actualResult = await getFirstPokemon()
+
+//   expect(actualResult).toEqual(expectedResult)
+// })
